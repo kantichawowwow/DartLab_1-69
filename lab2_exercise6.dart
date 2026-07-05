d446f2cdd5b24931a0195178faa2b5a4103aa465
kantichawowwow <kantichaf67@nu.ac.th>
2026-07-05 15:24:54 +0700

    Update lab2_exercise6.dart

--- lab2_exercise6.dart
+++ lab2_exercise6.dart
@@ -1,11 +1,26 @@
-void executeOperation(int a, int b, int Function(int, int) operation) {
+void executeOperation(double a, double b, double Function(double, double) operation) {
   var result = operation(a, b);
   print('Result: $result');
 }
 
-int square(int x, int y){
+double squareArea(double x, double y){
   return x*y;
 }
-int triangle(int x, int y){
+double triangleArea(double x, double y){
   return 0.5*x*y;
+}
+
+double cylinder(double radius, double height){
+  return 3.14*(radius*radius)*height;
+}
+
+void main(){
+  print("พื้นที่รูปสี่เหลี่ยม");
+  executeOperation(10, 5, squareArea);
+  print("พื้นที่รูปสามเหลี่ยม");
+  executeOperation(10, 5, triangleArea);
+  print("ปริมาตรทรงกระบอก");
+  double r = 3;
+  double h = 4;
+  print('${cylinder(r, h)}');
 }
